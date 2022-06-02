2062b8177444acb46478e1178d1ea487552090bb
HonzaVrbensky <91247842+HonzaVrbensky@users.noreply.github.com>
2022-06-02 09:37:37 +0200

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -3,26 +3,29 @@ radio.set_transmit_power(7)
 OVLADANI = False
 P1 = 1000
 P2 = 1000
+speed_1 = -200
+speed_2 = 0
 def on_button_pressed_a():
     print(P1)
     print(P2)
 input.on_button_pressed(Button.A, on_button_pressed_a)
+
 def follow_line():
     global P1, P2
     P1 = pins.analog_read_pin(AnalogPin.P0)
     P2 = pins.analog_read_pin(AnalogPin.P2)
     
     if P1 < 100 and P2 > 200:
-            PCAmotor.motor_run(PCAmotor.Motors.M2, -200)
-            PCAmotor.motor_run(PCAmotor.Motors.M1, -0)
+            PCAmotor.motor_run(PCAmotor.Motors.M2, speed_1)
+            PCAmotor.motor_run(PCAmotor.Motors.M1, speed_2)
             
     elif P1 > 100 and P2 < 200:
-            PCAmotor.motor_run(PCAmotor.Motors.M2, 0)
-            PCAmotor.motor_run(PCAmotor.Motors.M1, -200)
+            PCAmotor.motor_run(PCAmotor.Motors.M2, speed_2)
+            PCAmotor.motor_run(PCAmotor.Motors.M1, speed_1)
             
     else:
-            PCAmotor.motor_run(PCAmotor.Motors.M1, -200)
-            PCAmotor.motor_run(PCAmotor.Motors.M2, -200)
+            PCAmotor.motor_run(PCAmotor.Motors.M1, speed_1)
+            PCAmotor.motor_run(PCAmotor.Motors.M2, speed_1)
            
     print(P1+" "+P2)
 basic.forever(follow_line)
--- main.ts
+++ main.ts
@@ -3,6 +3,8 @@ radio.setTransmitPower(7)
 let OVLADANI = false
 let P1 = 1000
 let P2 = 1000
+let speed_1 = -200
+let speed_2 = 0
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     console.log(P1)
     console.log(P2)
@@ -12,14 +14,14 @@ basic.forever(function follow_line() {
     P1 = pins.analogReadPin(AnalogPin.P0)
     P2 = pins.analogReadPin(AnalogPin.P2)
     if (P1 < 100 && P2 > 200) {
-        PCAmotor.MotorRun(PCAmotor.Motors.M2, -200)
-        PCAmotor.MotorRun(PCAmotor.Motors.M1, -0)
+        PCAmotor.MotorRun(PCAmotor.Motors.M2, speed_1)
+        PCAmotor.MotorRun(PCAmotor.Motors.M1, speed_2)
     } else if (P1 > 100 && P2 < 200) {
-        PCAmotor.MotorRun(PCAmotor.Motors.M2, 0)
-        PCAmotor.MotorRun(PCAmotor.Motors.M1, -200)
+        PCAmotor.MotorRun(PCAmotor.Motors.M2, speed_2)
+        PCAmotor.MotorRun(PCAmotor.Motors.M1, speed_1)
     } else {
-        PCAmotor.MotorRun(PCAmotor.Motors.M1, -200)
-        PCAmotor.MotorRun(PCAmotor.Motors.M2, -200)
+        PCAmotor.MotorRun(PCAmotor.Motors.M1, speed_1)
+        PCAmotor.MotorRun(PCAmotor.Motors.M2, speed_1)
     }
     
     console.log(P1 + " " + P2)
